63d53050770134a29540947b0724f226fd5b48e3
diptasahacse <diptasaha.lpu.cse@gmail.com>
2022-03-29 22:11:18 +0600

    seperate name and value of slugs

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
+
+  const [phones, setPhones] = useState([])
+  useEffect(()=>{
+    axios.get('https://openapi.programming-hero.com/api/phones?search=iphone')
+    .then(myData => {
+      const loadedData = myData.data.data;
+      const phoneData = loadedData.map(phone => {
+        let phoneSlugs =  phone.slug.split('-');
+        let info = {
+          name: phoneSlugs[0],
+          value: phoneSlugs[1]
+        }
+        return info;
+      }
+     
+    
+      
+      );
+      console.log(phoneData)
+    })
+  },[])
+  console.log(phones)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
